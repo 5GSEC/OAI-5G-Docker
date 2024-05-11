0eca778dc523474e19eb9ff22a15ce1d147e8407
onehouwong <onehouwong@gmail.com>
2024-05-11 13:49:57 -0400

    Update kill.sh

--- nr-rfsim/kill.sh
+++ nr-rfsim/kill.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker kill oai-ext-dn oai-spgwu oai-smf oai-amf oai-nrf mysql #$(docker ps -a -q)
+docker rm oai-ext-dn oai-spgwu oai-smf oai-amf oai-nrf mysql #$(docker ps -a -q)
