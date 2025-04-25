0ad61de7a1c8b9aae538d38726bae3449e3336c7
onehouwong <onehouwong@gmail.com>
2025-04-25 13:13:29 -0400

    add demo

---
+++ navy-demo/nr-rfsim/kill_attack_ue.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker kill rfsim5g-oai-attack-ue-0 rfsim5g-oai-attack-ue-1
+docker rm rfsim5g-oai-attack-ue-0 rfsim5g-oai-attack-ue-1
---
+++ navy-demo/nr-rfsim/kill_ran.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker kill rfsim5g-oai-du rfsim5g-oai-cu
+docker rm rfsim5g-oai-du rfsim5g-oai-cu
